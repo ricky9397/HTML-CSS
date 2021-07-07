923d9a8bf102ee37c1676c040c8a48f8e0cbf962
ricky9397 <kimzzan1234@gmail.com>
2021-07-07 13:21:31 +0900

    js 문법공부
    js 문법공부

---
+++ javascript/json.js
@@ -0,0 +1,24 @@
+// JSON
+// JavaScript Object Notation
+
+// 1. Object to JSON
+// stringfy(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+    name: 'tori',
+    color: 'white',
+    size: null,
+    birthDate: new Date(),
+    jump: () => {
+        console.log('${name} can jump!');
+    },
+
+}
+
+// 2. JSON to Object
+// parse(json)
